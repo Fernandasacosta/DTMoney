b9a73815ff0ad51cd6f46e7023f85db6e6e1e758
Fernandasacosta <fernandasacostaa@gmail.com>
2025-02-04 08:36:00 -0300

    Summary Component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transactions(){
   return(
     <>
       <Header/>
-      <h1>Transactios</h1>
+      <Summary/>
+      
     </>
     
   )
